7b370c510e8057571f9fcddaa3b7f20791ae6876
juddyblazzy06 <amiensjude@gmail.com>
2025-10-29 21:51:54 +0100

    feat(setup): Create React app with Vite, install dependencies, and set up project structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# Web3Bridge Number Guesser Game
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Number Guesser Game</h1>
+      <p>Game components will be added here</p>
+    </div>
+  )
+}
+
+export default App
+
